90b639c3271ab81b3109c2c2af845ed265162790
Carl Noval <cjrnoval22@gmail.com>
2023-02-04 01:24:48 +0900

    Update: Changed default URL, Update: Asjusted tests to pass with new URL

--- tests/end-to-end/sample-tests/example.spec.ts
+++ tests/end-to-end/sample-tests/example.spec.ts
@@ -4,15 +4,15 @@ test('has title', async ({ page }) => {
   await page.goto('');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("The Internet");
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Form Authentication' }).click();
 
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await expect(page).toHaveURL(/\/login/);
 });
