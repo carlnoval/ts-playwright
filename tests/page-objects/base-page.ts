68f63e163f1bf4029749a2c4609d5b9fa312c9ba
Carl Noval <cjrnoval22@gmail.com>
2023-02-04 23:45:21 +0900

    Add: Base page draft

---
+++ tests/page-objects/base-page.ts
@@ -0,0 +1,39 @@
+import { Page } from "@playwright/test";
+import { FormAuthPage } from "./form-auth-page";
+
+export class BasePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page  = page;
+  }
+
+  async clickFormAuthentication() {
+    await this.clickLink("Form Authentication");
+    return new FormAuthPage(this.page);
+  } 
+
+  private async clickLink(linkText: string) {
+    await this.page.getByRole('link', { name: linkText }).click()
+  }
+
+  // public LoginPage clickFormAuthentication() {
+  //   clickLink("Form Authentication");
+  //   return new LoginPage(driver);
+  // }
+
+  // public KeyPressesPage clickKeyPressesPage() {
+  //     clickLink("Key Presses");
+  //     return new KeyPressesPage(driver);
+  // }
+
+  // public ChallengingDOM clickChallengingDOM() {
+  //     clickLink("Challenging DOM");
+  //     return new ChallengingDOM(driver);
+  // }
+
+  // // helper method prevents repetition of multiple locators
+  // private void clickLink(String linkText) {
+  //     driver.findElement(By.linkText(linkText)).click();
+  // }
+}
