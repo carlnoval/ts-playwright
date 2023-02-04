aeaf79c6d14db734e94c1445be228fd4d3dacece
Carl Noval <cjrnoval22@gmail.com>
2023-02-05 01:33:29 +0900

    Update: Example test now uses POM and fixture

--- tests/end-to-end/sample-tests/example.spec.ts
+++ tests/end-to-end/sample-tests/example.spec.ts
@@ -1,17 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { testY, expect } from '../../utils/fixtures/base-page-fixture'
 
-test('has title', async ({ page }) => {
-  await page.goto('');
-
+// base-page-fixture has _basePage automatically executed so no need to use it along with page
+testY('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("The Internet");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('');
-
+// though base-page-fixture is automatically executed, still need to explicitly use it to access its method
+testY('navigate to login page', async ({ _basePage, page }) => {
   // Click the get started link.
-  await page.getByRole('link', { name: 'Form Authentication' }).click();
+  await _basePage.clickFormAuthentication();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/\/login/);
