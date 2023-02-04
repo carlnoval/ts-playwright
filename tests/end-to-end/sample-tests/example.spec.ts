62e07a945a427fdabb68f4835560c8867c9d8f3c
Carl Noval <cjrnoval22@gmail.com>
2023-02-05 02:09:23 +0900

    Update: Removed object type since only one fixture is on the file, Update: Comment improvement on fixture cleanup

--- tests/end-to-end/sample-tests/example.spec.ts
+++ tests/end-to-end/sample-tests/example.spec.ts
@@ -1,16 +1,16 @@
-import { testY, expect } from '../../utils/fixtures/base-page-fixture'
+import { testX, expect } from '../../utils/fixtures/base-page-fixture'
 
-// base-page-fixture has _basePage automatically executed so no need to use it along with page
-testY('has title', async ({ page }) => {
+// _basePage fixture is set to auto execute
+testX('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("The Internet");
 });
 
-// though base-page-fixture is automatically executed, still need to explicitly use it to access its method
-testY('navigate to login page', async ({ _basePage, page }) => {
+// tough _basePage fixture is set to auto execute, still need to explicitly call it to access its method
+testX('navigate to login page', async ({ _basePage, page }) => {
   // Click the get started link.
   await _basePage.clickFormAuthentication();
 
-  // Expects the URL to contain intro.
+  // Expects the URL to contain '/login/'.
   await expect(page).toHaveURL(/\/login/);
 });
