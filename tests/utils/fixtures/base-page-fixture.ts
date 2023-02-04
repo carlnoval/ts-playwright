e7dbd28e08b7988bc3d245498e5d8bb1871cc292
Carl Noval <cjrnoval22@gmail.com>
2023-02-05 01:29:11 +0900

    Update: Completed fixture for base page

--- tests/utils/fixtures/base-page-fixture.ts
+++ tests/utils/fixtures/base-page-fixture.ts
@@ -1,2 +1,24 @@
+import { test as testX } from '@playwright/test';
 import { BasePage } from "../../page-objects/base-page";
 
+// Declare the types of your fixtures.
+type basePageFixtures = {
+  _basePage: BasePage;
+};
+
+export const testY = testX.extend<basePageFixtures>({
+  _basePage: [async ({ page }, use) => {
+    // Set up the fixture.
+    await page.goto('');
+    const todoPage = new BasePage(page);
+
+    // Use the fixture value in the test.
+    await use(todoPage);
+
+    // Code for fixture cleanup, none needed for now
+
+    // `auto: true` allows automatic usage of this fixture if testY is used
+  }, { auto: true }]
+});
+
+export { expect } from '@playwright/test';
