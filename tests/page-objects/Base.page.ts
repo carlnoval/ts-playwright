907147192bbb4bfb598eea4a3ec0b7141fb99739
Carl Noval <cjrnoval22@gmail.com>
2023-02-06 21:23:44 +0900

    Removed: Reference comments

--- tests/page-objects/Base.page.ts
+++ tests/page-objects/Base.page.ts
@@ -21,24 +21,4 @@ export class BasePage {
   private async clickLink(linkText: string) {
     await this.page.getByRole('link', { name: linkText }).click()
   }
-
-  // public LoginPage clickFormAuthentication() {
-  //   clickLink("Form Authentication");
-  //   return new LoginPage(driver);
-  // }
-
-  // public KeyPressesPage clickKeyPressesPage() {
-  //     clickLink("Key Presses");
-  //     return new KeyPressesPage(driver);
-  // }
-
-  // public ChallengingDOM clickChallengingDOM() {
-  //     clickLink("Challenging DOM");
-  //     return new ChallengingDOM(driver);
-  // }
-
-  // // helper method prevents repetition of multiple locators
-  // private void clickLink(String linkText) {
-  //     driver.findElement(By.linkText(linkText)).click();
-  // }
 }
