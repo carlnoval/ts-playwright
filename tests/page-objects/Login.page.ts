dd7e9497e174b820bddf853654939ad87879f6c6
Carl Noval <cjrnoval22@gmail.com>
2023-02-06 17:12:02 +0900

    Add: Login page object

--- tests/page-objects/Login.page.ts
+++ tests/page-objects/Login.page.ts
@@ -1,9 +1,27 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
+import { SecurePage } from "./Secure.page";
+
 
 export class LoginPage {
   readonly page: Page;
+  readonly usernameField: Locator;
+  readonly passwordField: Locator;
+  readonly loginButton: Locator;
 
   constructor(page: Page) {
     this.page  = page;
+    this.usernameField = page.getByLabel('Username');
+    this.passwordField = page.getByLabel('Password');
+    this.loginButton = page.getByRole('button', { name: /. Login/ })
+  }
+
+  async enterLogin(username: string, password: string) {
+    await this.usernameField.fill(username);
+    await this.passwordField.fill(password);
+  }
+
+  async clickLoginButton() {
+    await this.loginButton.click();
+    return new SecurePage(this.page);
   }
 }
