127b5ecfdeec81ad699a49f6d80b3949cc929a6d
Carl Noval <cjrnoval22@gmail.com>
2023-02-06 21:24:15 +0900

    Add: Baremin key-presses test

---
+++ tests/end-to-end/key-presses.spec.ts
@@ -0,0 +1,14 @@
+import { expect, testX } from '../utils/fixtures/_basePage.fixture';
+
+testX('navigate to login page', async ({ _basePage }) => {
+  // From: console.log(JSON.stringify(await securePage.getBannerMessage()))
+  const RAWBANNERMESSAGE = "\n            You logged into a secure area!\n            ×\n          ";
+  const USERNMAE = "tomsmith";
+  const PASSWORD = "SuperSecretPassword!";
+
+  const loginPage = await _basePage.clickFormAuthentication();                  // Navigate to form auth page
+  await loginPage.enterLogin(USERNMAE, PASSWORD);                               // Provide credentials
+  const securePage = await loginPage.clickLoginButton();                        // Proceed to login
+
+  expect(await securePage.getWelcomeBannerMessage()).toEqual(RAWBANNERMESSAGE)  // Verify that the right success 
+});
