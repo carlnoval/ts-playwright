9bd033f5163f45fda109d5c730827963ce6f64bc
Carl Noval <cjrnoval22@gmail.com>
2023-02-06 17:12:14 +0900

    Add: Secure page object

---
+++ tests/page-objects/Secure.page.ts
@@ -0,0 +1,15 @@
+import { Locator, Page } from "@playwright/test";
+
+export class SecurePage {
+  readonly page: Page;
+  readonly loginBannerMessage: Locator;
+
+  constructor(page: Page) {
+    this.page  = page;
+    this.loginBannerMessage = page.getByText('You logged into a secure area! ×')
+  }
+
+  async getWelcomeBannerMessage() {
+    return await this.loginBannerMessage.textContent();
+  }
+}
