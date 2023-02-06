105e8efe7678f4e9febe1fee06e0a537dc05d4dd
Carl Noval <cjrnoval22@gmail.com>
2023-02-07 01:56:00 +0900

    Update: Key Presses page required locator and methods

--- tests/page-objects/KeyPresses.page.ts
+++ tests/page-objects/KeyPresses.page.ts
@@ -1,9 +1,31 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class KeyPresses {
   readonly page: Page;
+  readonly textField: Locator;
+  readonly resultMessageDisplay: Locator;
 
   constructor(page: Page) {
     this.page  = page;
+    this.textField = page.locator('#target');
+    this.resultMessageDisplay = page.locator('#result')
+  }
+
+  async typeKeys(str: string) {
+    this.clearTextField();
+    await this.textField.type(str);
+  }
+  
+  async pressSpecialKey(specialKey: string) {
+    this.clearTextField();
+    await this.textField.press(specialKey);
+  }
+
+  async getResultMessage() {
+    return await this.resultMessageDisplay.textContent();
+  }
+
+  private async clearTextField() {
+    await this.textField.fill("");
   }
 }
