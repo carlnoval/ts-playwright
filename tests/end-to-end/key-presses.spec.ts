0e2d1f81d5482918584085834646b8626b5db613
Carl Noval <cjrnoval22@gmail.com>
2023-02-07 12:09:13 +0900

    Update: Segregated common vs special key tests, Update: Better describe

--- tests/end-to-end/key-presses.spec.ts
+++ tests/end-to-end/key-presses.spec.ts
@@ -1,11 +1,15 @@
 import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 
-testX('navigate to login page', async ({ _basePage }) => {
+testX('special key press', async ({ _basePage }) => {
+  const keyPressesPage = await _basePage.clickKeyPressesPage();
+
+  await keyPressesPage.pressSpecialKey('Delete');
+  expect(await keyPressesPage.getResultMessage()).toBe("You entered: DELETE");
+});
+
+testX('common key press', async ({ _basePage }) => {
   const keyPressesPage = await _basePage.clickKeyPressesPage();
 
   await keyPressesPage.typeKeys('pi=3.14');
   expect(await keyPressesPage.getResultMessage()).toBe("You entered: 4");
-
-  await keyPressesPage.pressSpecialKey('Delete');
-  expect(await keyPressesPage.getResultMessage()).toBe("You entered: DELETE");
 });
