b18ee37ad72b88b4a2e61815dd71a5c3768e95ac
Carl Noval <cjrnoval22@gmail.com>
2023-02-07 16:02:55 +0900

    Update: Used 'await exptect' + web first assertion

--- tests/end-to-end/login.spec.ts
+++ tests/end-to-end/login.spec.ts
@@ -1,13 +1,13 @@
 import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 
 testX('login - valid credentials', async ({ _basePage }) => {
-  // From: console.log(JSON.stringify(await securePage.getBannerMessage()))
+  // From: console.log(JSON.stringify(await securePage.loginBannerMessage.textContent()))
   const RAWBANNERMESSAGE = "\n            You logged into a secure area!\n            ×\n          ";
   const USERNMAE = "tomsmith";
   const PASSWORD = "SuperSecretPassword!";
 
-  const loginPage = await _basePage.clickFormAuthentication();                  // Navigate to form auth page
-  await loginPage.enterLogin(USERNMAE, PASSWORD);                               // Provide credentials
-  const securePage = await loginPage.clickLoginButton();                        // Proceed to login
-  expect(await securePage.getWelcomeBannerMessage()).toEqual(RAWBANNERMESSAGE)  // Verify that the right success 
+  const loginPage = await _basePage.clickFormAuthentication();                // Navigate to form auth page
+  await loginPage.enterLogin(USERNMAE, PASSWORD);                             // Provide credentials
+  const securePage = await loginPage.clickLoginButton();                      // Proceed to login
+  await expect(securePage.loginBannerMessage).toHaveText(RAWBANNERMESSAGE)    // Verify that the right success 
 });
