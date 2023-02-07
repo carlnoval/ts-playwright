7bfa116f45c4c909fe2f791195f86a0ee78f87bb
Carl Noval <cjrnoval22@gmail.com>
2023-02-08 02:30:30 +0900

    Add: clickBrokenImagesLink() method, Update: Method names to use clickPageNameLink format

--- utils/page-objects/Base.page.ts
+++ utils/page-objects/Base.page.ts
@@ -1,4 +1,5 @@
 import { Page } from "@playwright/test";
+import { BrokenImagesPage } from "./BrokenImages.page";
 import { LoginPage } from "./Form Authentication Pages/Login.page";
 import { KeyPressesPage } from "./KeyPresses.page";
 
@@ -9,14 +10,19 @@ export class BasePage {
     this.page  = page;
   }
 
-  async clickFormAuthentication() {
+  async clickFormAuthenticationLink() {
     await this.clickLink("Form Authentication");
     return new LoginPage(this.page);
   }
 
-  async clickKeyPressesPage() {
+  async clickKeyPressesLink() {
     await this.clickLink("Key Presses");
     return new KeyPressesPage(this.page);
+  }
+
+  async clickBrokenImagesLink() {
+    await this.clickLink("Broken Images");
+    return new BrokenImagesPage(this.page);
   }
 
   private async clickLink(linkText: string) {
