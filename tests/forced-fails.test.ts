2fc2ae002b091d7447cd61655aed1fd636b7f272
Carl Noval <cjrnoval22@gmail.com>
2023-02-08 14:00:48 +0900

    Update: Completed force failed page visual test

--- tests/forced-fails.test.ts
+++ tests/forced-fails.test.ts
@@ -1,11 +1,16 @@
 import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 
-testX('text does not match', async ({ _basePage }) => {
+testX.skip('text does not match', async ({ _basePage }) => {
   const WRONGHEADERTEXT = 'xWelcome to the-internet';
-  
+
   await expect(_basePage.pageHeader).toHaveText(WRONGHEADERTEXT);
 });
 
-testX.skip('element image does not match', async ({ _basePage }) => {
+testX('element image does not match', async ({ _basePage }) => {
+  const shiftingContentIndexPage = await _basePage.clickShiftingContextIndexLink();
+  const shiftingImagePage = await shiftingContentIndexPage.clickShiftingImagePageLink();
 
+  await shiftingImagePage.page.reload();                    // Site was designed to shift the image position after reloads
+  await expect(shiftingImagePage.pageImage).toBeVisible();  // Ensures that page screenshot will include the image
+  await expect(shiftingImagePage.page).toHaveScreenshot();
 });
