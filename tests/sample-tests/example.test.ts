5a1857e2ab4a42b53c2ad2e0133914d8039ab6ca
Carl Noval <cjrnoval22@gmail.com>
2023-02-08 12:57:49 +0900

    Update: Skipping sample-tests

--- tests/sample-tests/example.test.ts
+++ tests/sample-tests/example.test.ts
@@ -1,13 +1,13 @@
 import { expect, testX } from '../../utils/fixtures/_basePage.fixture';
 
 // _basePage fixture is set to auto execute
-testX('has title', async ({ page }) => {
+testX.skip('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("The Internet");
 });
 
 // tough _basePage fixture is set to auto execute, still need to explicitly call it to access its method
-testX('navigate to login page', async ({ _basePage, page }) => {
+testX.skip('navigate to login page', async ({ _basePage, page }) => {
   // Click the get started link.
   await _basePage.clickFormAuthenticationLink();
 
