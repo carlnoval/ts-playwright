f59d3320bebc25d3f1c5dc948e20b4b04b76c0dd
Carl Noval <cjrnoval22@gmail.com>
2023-02-08 14:21:36 +0900

    Update: Troubleshooting firefox fails, undoing skip test removal from previous commit

--- tests/forced-fails.test.ts
+++ tests/forced-fails.test.ts
@@ -1,6 +1,6 @@
 import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 
-testX('text does not match', async ({ _basePage }) => {
+testX.skip('text does not match', async ({ _basePage }) => {
   const WRONGHEADERTEXT = 'xWelcome to the-internet';
 
   await expect(_basePage.pageHeader).toHaveText(WRONGHEADERTEXT);
