5a4939650f162519101f650e4b337cd68b66018a
Carl Noval <cjrnoval22@gmail.com>
2023-02-08 12:51:08 +0900

    Update: Improved comment

--- tests/image.test.ts
+++ tests/image.test.ts
@@ -2,6 +2,6 @@ import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 
 testX('element screenshot', async ({ _basePage }) => {
   const brokenImagePage = await _basePage.clickBrokenImagesLink();
-  // await expect(brokenImagePage.imagesContainer).toHaveScreenshot(['foo','bar','jag.png'])
+  // Assertion can have args toHaveScreenshot(['folder1','foler2','screenshotName.png'])
   await expect(brokenImagePage.imagesContainer).toHaveScreenshot();
 });
