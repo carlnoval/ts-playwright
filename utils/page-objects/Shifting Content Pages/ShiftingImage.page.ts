1a7b4f7b9dd9657ad8cee22d474aaf2e67a96f4f
Carl Noval <cjrnoval22@gmail.com>
2023-02-09 17:53:20 +0900

    Update: Used image locator and cotainer locator

--- utils/page-objects/Shifting Content Pages/ShiftingImage.page.ts
+++ utils/page-objects/Shifting Content Pages/ShiftingImage.page.ts
@@ -1,12 +1,13 @@
 import { Locator, Page } from "@playwright/test";
 
-
 export class ShiftingImagePage {
   readonly page: Page;
+  readonly pageContent: Locator;
   readonly pageImage: Locator;
 
   constructor(page: Page) {
     this.page  = page;
+    this.pageContent = page.locator('#content');
     this.pageImage = page.locator('#content').getByRole('img');
   }
 }
