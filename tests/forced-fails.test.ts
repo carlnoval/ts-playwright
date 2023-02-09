3b44c9f5c7ae331d5173caee70c2cf43f058fb2e
Carl Noval <cjrnoval22@gmail.com>
2023-02-09 17:55:35 +0900

    Update: Dedicated 1 visual test for element and for entire page

--- tests/forced-fails.test.ts
+++ tests/forced-fails.test.ts
@@ -2,15 +2,21 @@ import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 
 testX('text does not match', async ({ _basePage }) => {
   const WRONGHEADERTEXT = 'xWelcome to the-internet';
-
   await expect(_basePage.pageHeader).toHaveText(WRONGHEADERTEXT);
 });
 
 testX('element image does not match', async ({ _basePage }) => {
   const shiftingContentIndexPage = await _basePage.clickShiftingContextIndexLink();
   const shiftingImagePage = await shiftingContentIndexPage.clickShiftingImagePageLink();
+  // Site was designed to shift the image position after reload
+  await shiftingImagePage.page.reload();
+  // Ensures that the image in the page exists before takeing screenshot
+  await expect(shiftingImagePage.pageImage).toBeVisible();
+  await expect(shiftingImagePage.pageContent).toHaveScreenshot();
+});
 
-  await shiftingImagePage.page.reload();                    // Site was designed to shift the image position after reloads
-  await expect(shiftingImagePage.pageImage).toBeVisible();  // Ensures that page screenshot will include the image
-  await expect(shiftingImagePage.page).toHaveScreenshot();
+testX('page image does not match', async ({ _basePage }) => {
+  // page was designed to have unique visuals on every load
+  const challengingDOMPage = await _basePage.clickChallengingDOMLink();
+  await expect(challengingDOMPage.page).toHaveScreenshot();
 });
