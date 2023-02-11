df2b1171f19099ccce72b9016f32629d934108db
Carl Noval <cjrnoval22@gmail.com>
2023-02-12 00:24:34 +0900

    Update: Match with screenshots with defined names

--- tests/forced-fails.test.ts
+++ tests/forced-fails.test.ts
@@ -10,15 +10,16 @@ testX.describe('Tests that are expected to fail', () => {
     const shiftingContentIndexPage = await _basePage.clickShiftingContextIndexLink();
     const shiftingImagePage = await shiftingContentIndexPage.clickShiftingImagePageLink();
     // Site was designed to shift the image position after reload
+    // Comment reload step when retaking screenshots
     await shiftingImagePage.page.reload();
     // Ensures that the image in the page exists before takeing screenshot
     await expect(shiftingImagePage.pageImage).toBeVisible();
-    await expect(shiftingImagePage.pageContent).toHaveScreenshot();
+    await expect(shiftingImagePage.pageContent).toHaveScreenshot('pageContent.png');
   });
 
   testX('page image does not match', async ({ _basePage }) => {
     // page was designed to have unique visuals on every load
     const challengingDOMPage = await _basePage.clickChallengingDOMLink();
-    await expect(challengingDOMPage.page).toHaveScreenshot();
+    await expect(challengingDOMPage.page).toHaveScreenshot('page.png');
   });
 });
