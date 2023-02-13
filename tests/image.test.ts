69ca4eedc51c8a56d5a970a1bb9a8f5a27f8dc09
Carl Noval <cjrnoval22@gmail.com>
2023-02-13 18:19:13 +0900

    Add: Page screenshot test for LargeDeepDom page

--- tests/image.test.ts
+++ tests/image.test.ts
@@ -7,4 +7,21 @@ testX.describe('Visual Test', () => {
     // Assertion can have args toHaveScreenshot(['folder1','foler2','screenshotName.png'])
     await expect(brokenImagePage.imagesContainer).toHaveScreenshot('imagesContainer.png');
   });
+
+  testX('page screenshot',async ({ _basePage }) => {
+    const largeDeepDOMPage = await _basePage.clickLargeDeepDOMLink();
+    await expect(largeDeepDOMPage.largeTable).toBeVisible();
+    await expect(largeDeepDOMPage.siblingDivs).toBeVisible();
+    // Capturing the entire table is flaky so only capturing the header part which have been stable
+    const screenshotOptions = {
+      fullPage: true,
+      clip: {
+        x: 0,
+        y: 0,
+        width: 2700,
+        height: 2750
+      }
+    }
+    await expect(largeDeepDOMPage.page).toHaveScreenshot('entire-page.png', screenshotOptions);
+  })
 });
