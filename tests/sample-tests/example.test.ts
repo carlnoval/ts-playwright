ae45dee09f6dc3e4500834059ca6808a99319da2
Carl Noval <cjrnoval22@gmail.com>
2023-02-13 15:30:58 +0900

    Update: Commented long demo test

--- tests/sample-tests/example.test.ts
+++ tests/sample-tests/example.test.ts
@@ -1,14 +1,14 @@
 import { expect, testX } from '../../utils/fixtures/_basePage.fixture';
 
-testX.describe('Basic validations', () => {
+testX.describe.skip('Basic validations', () => {
   // _basePage fixture is set to auto execute
-  testX.skip('has title', async ({ page }) => {
+  testX('has title', async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle("The Internet");
   });
 
   // tough _basePage fixture is set to auto execute, still need to explicitly call it to access its method
-  testX.skip('navigate to login page', async ({ _basePage, page }) => {
+  testX('navigate to login page', async ({ _basePage, page }) => {
     // Click the get started link.
     await _basePage.clickFormAuthenticationLink();
 
