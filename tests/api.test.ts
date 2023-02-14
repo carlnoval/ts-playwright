2f3ec3e2e2d3615359666cf7a99dbe1a1d942d3d
Carl Noval <cjrnoval22@gmail.com>
2023-02-14 16:22:28 +0900

    Update: Console log to see value of env variable

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -3,5 +3,6 @@ import { expect, testX } from '../utils/fixtures/_basePage.fixture';
 testX.describe('API Tests', () => {
   testX('api test1', async () => {
     expect(process.env.DUMMY_TOKEN === "The Dummy Token").toBeTruthy();
+    console.log(process.env.DUMMY_TOKEN);
   });
 });
