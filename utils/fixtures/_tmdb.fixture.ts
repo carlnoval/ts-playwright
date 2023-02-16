6c46f89b9814cc8d8dc2423c6165a6641a8afa05
Carl Noval <cjrnoval22@gmail.com>
2023-02-16 19:58:36 +0900

    Update: Use a dedicated type to make fixtures more readable

--- utils/fixtures/_tmdb.fixture.ts
+++ utils/fixtures/_tmdb.fixture.ts
@@ -1,8 +1,14 @@
 import { test } from '@playwright/test';
 
-export const testY = test.extend<{ _tmdbV3APIKey : string, _tmdbV3API : string }>({
-  _tmdbV3APIKey : process.env.TMDB_API_V3_KEY || 'key_not_detected',
-  _tmdbV3API : 'https://api.themoviedb.org/3/'
+type TestFixtures = {
+  _tmdbV3APIKey: string,
+  _tmdbV3API: string,
+};
+
+export const testY = test.extend<TestFixtures>({
+  // Variable fixtures does not need 'use', functional fixtures needs it.
+  _tmdbV3APIKey: process.env.TMDB_API_V3_KEY || 'key_not_detected',
+  _tmdbV3API: 'https://api.themoviedb.org/3/',
 });
 
 export { expect } from '@playwright/test';
