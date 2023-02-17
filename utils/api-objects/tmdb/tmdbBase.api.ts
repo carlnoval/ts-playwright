9ae734e929082c6444467b821087ff36d2ceaf60
Carl Noval <cjrnoval22@gmail.com>
2023-02-18 03:05:44 +0900

    Add: tmdb base API class

---
+++ utils/api-objects/tmdb/tmdbBase.api.ts
@@ -0,0 +1,18 @@
+import { APIRequestContext } from "@playwright/test";
+
+export class TMDBBaseAPI {
+  readonly request: APIRequestContext;
+  readonly tmdbAPIURL: string;
+  readonly tmdbV3APIKey: string;
+
+  /**
+   * Sensitive key/secrets are not tracked on the repo.
+   * However, key/secrets will still be revealed in playwright/allure report and trace.
+   * Issue that was closed: https://github.com/microsoft/playwright/issues/19992
+   */
+  constructor(request: APIRequestContext) {
+    this.request = request;
+    this.tmdbAPIURL = "https://api.themoviedb.org/3";
+    this.tmdbV3APIKey = `?api_key=${process.env.TMDB_API_V3_KEY}`
+  }
+}
