17423d88fa94b47c1fac5015bc8c8a64336b04fa
Carl Noval <cjrnoval22@gmail.com>
2023-02-18 03:07:45 +0900

    Update: Failing API tests to use new tmdb fixture

--- tests/forced-API-fails.test.ts
+++ tests/forced-API-fails.test.ts
@@ -1,28 +1,22 @@
 import { expect, testY } from '../utils/fixtures/_tmdb.fixture';
 
 testY.describe('API Tests - forced to fail', () => {
-  testY('status does not match', async ({request, _tmdbV3API, _tmdbV3APIKey}) => {
-    const validID = '315162'
-    const requestURL = `${_tmdbV3API}movie/${validID}?api_key=${_tmdbV3APIKey}` ;
-    const movie = await request.get(requestURL);
-    expect(movie.status()).toBe(201);
+  testY('status does not match', async ({ _tmdbMovieEndpoint}) => {
+    const requestedMovie = await _tmdbMovieEndpoint.GetMovie(315162);
+    expect(requestedMovie.status()).toBe(201);
   });
   
   // Invalid movie id api response is: { success: false, status_code: 34, status_message: 'The resource you requested could not be found.' }
-  testY('key value pair does not match - array & toContainEqual', async ({request, _tmdbV3API, _tmdbV3APIKey}) => {
-    const invalidID = '999999999999';
-    const requestURL = `${_tmdbV3API}movie/${invalidID}?api_key=${_tmdbV3APIKey}`;
-    const movie = await request.get(requestURL);
-    expect([await movie.json()]).toContainEqual(expect.objectContaining({
+  testY('non matching key value pair - array & toContainEqual', async ({ _tmdbMovieEndpoint }) => {
+    const requestedMovie = await _tmdbMovieEndpoint.GetMovie(9999999999);
+    expect([await requestedMovie.json()]).toContainEqual(expect.objectContaining({
       success: true
     }));
   });
   
-  testY('key does not exist - json & toEqual', async ({request, _tmdbV3API, _tmdbV3APIKey}) => {
-    const invalidID = '999999999999';
-    const requestURL = `${_tmdbV3API}movie/${invalidID}?api_key=${_tmdbV3APIKey}`;
-    const movie = await request.get(requestURL);
-    expect(await movie.json()).toEqual(expect.objectContaining({
+  testY('key does not exist - json & toEqual', async ({ _tmdbMovieEndpoint }) => {
+    const requestedMovie = await _tmdbMovieEndpoint.GetMovie(9999999999);
+    expect(await requestedMovie.json()).toEqual(expect.objectContaining({
       success: false,
       status_code: 34,
       status_message: "The resource you requested could not be found.",
