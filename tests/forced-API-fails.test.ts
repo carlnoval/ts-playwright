b99a1eef19856ed77e7e2c36e5ce6e394fe32182
Carl Noval <cjrnoval22@gmail.com>
2023-02-17 21:01:39 +0900

    Update: Dedicated failed tests for API

---
+++ tests/forced-API-fails.test.ts
@@ -0,0 +1,32 @@
+import { expect, testY } from '../utils/fixtures/_tmdb.fixture';
+
+testY.describe('API Tests - forced to fail', () => {
+  testY('status does not match', async ({request, _tmdbV3API, _tmdbV3APIKey}) => {
+    const validID = '315162'
+    const requestURL = `${_tmdbV3API}movie/${validID}?api_key=${_tmdbV3APIKey}` ;
+    const movie = await request.get(requestURL);
+    expect(movie.status()).toBe(201);
+  });
+  
+  // Invalid movie id api response is: { success: false, status_code: 34, status_message: 'The resource you requested could not be found.' }
+  testY('key value pair does not match - array & toContainEqual', async ({request, _tmdbV3API, _tmdbV3APIKey}) => {
+    const invalidID = '999999999999';
+    const requestURL = `${_tmdbV3API}movie/${invalidID}?api_key=${_tmdbV3APIKey}`;
+    const movie = await request.get(requestURL);
+    expect([await movie.json()]).toContainEqual(expect.objectContaining({
+      success: true
+    }));
+  });
+  
+  testY('key does not exist - json & toEqual', async ({request, _tmdbV3API, _tmdbV3APIKey}) => {
+    const invalidID = '999999999999';
+    const requestURL = `${_tmdbV3API}movie/${invalidID}?api_key=${_tmdbV3APIKey}`;
+    const movie = await request.get(requestURL);
+    expect(await movie.json()).toEqual(expect.objectContaining({
+      success: false,
+      status_code: 34,
+      status_message: "The resource you requested could not be found.",
+      non_existing_key: true
+    }));
+  });
+});
